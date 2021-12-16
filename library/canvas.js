08f08ac06f8e447355d4cb84baeda10a0f9e182b
Cornelis Drost <seregon888@gmail.com>
2021-12-16 18:10:17 +1300

    REC-4 Binary Zoom
    * Swap out automatically when canvas too small
    * Handle calculation of continuous zoom

--- library/canvas.js
+++ library/canvas.js
@@ -1,4 +1,6 @@
 class BinaryZoom {
+    // Allows infinitely zooming in and out of an image by swapping out canvases when they get
+    // too small (todo: or big)
     constructor(res, target) {
         this.res = res;
         this.active = target.createGraphics(this.res, this.res);
@@ -6,9 +8,18 @@ class BinaryZoom {
         this.t = target;
     }
 
-    swap(darken, label) {
+    /**
+     * Swaps out the current canvas to one at half the scale (twice the size), draws the current
+     * canvas to the center of the new one
+     * @param darken tint the canvas after swap, to visualise swapping
+     * @param label label the new canvas with its level
+     */
+    swap(darken = false, label = false) {
+        // Create new canvas and copy current image
         let img = this.t.createGraphics(this.res, this.res);
         img.image(this.active, this.res/4, this.res/4, this.res/2, this.res/2);
+
+        // Optionally tint and label the new canvas
         if (darken) {
             img.background(0,0,0,20)
         }
@@ -17,25 +28,35 @@ class BinaryZoom {
             img.textAlign(this.t.LEFT, this.t.TOP);
             img.text(this.level + 1, 10, 10);
         }
+
+        // Replace current canvas
         this.active.remove();
         this.active = img;
         this.level++;
     }
 
+    /**
+     * Draw the current canvas to target at given scale
+     * @param scale At 1.0, fits 1:1 within target, at 0.5, target is filled with half of active canvas
+     */
     draw(scale) {
-        // Draw current canvas such that
         let offset = (1 - scale) * 0.5 * this.res;
         let depth = this.res - 2 * offset;
         this.t.image(this.active, 0, 0, this.t.width, this.t.height, offset, offset, depth, depth);
     }
 
-    autodraw(N) {
+    /**
+     * Automatically scales current active canvas (swapping if necessary), and draws at correct scale
+     * @param N Linear scale factor, such that level 1 canvas fills 1 / 2^(N-2) of target window
+     */
+    auto_scale_draw(N) {
+        // Swap if necessary
         let target_level = this.t.floor(N);
         while (target_level > this.level) {
             this.swap(true, true);
         }
+        // Calculate appropriate scale and draw
         let scale = 1 / 2 ** (1 - N % 1);
-        // let scale = 1 / (2 - N % 1);
         this.draw(scale)
     }
 }
--- sketches/zoomTest.js
+++ sketches/zoomTest.js
@@ -1,24 +1,21 @@
 let sketch = function(p) {
     p.setup = function () {
-        p.createCanvas(500, 500);
+        p.createCanvas(800, 800);
         p.background(100);
-        // p.noLoop();
         p.canvas = new BinaryZoom(1000, p)
-        p.canvas.active.background(200);
-        // p.noLoop()
+
+        // Label and darken first level
+        p.canvas.active.textSize(p.canvas.res/10);
+        p.canvas.active.textAlign(p.LEFT, p.TOP);
+        p.canvas.active.text(1, 10, 10);
+        p.canvas.active.background(0,0,0,20);
     }
 
     p.draw = function () {
-        let N = p.millis() / 1000;
+        let N = 1 + p.millis() / 1000;
         p.background(100);
-        // p.canvas.draw(p, 1)
-        // p.canvas.swap(false, true)
-        p.canvas.autodraw(N);
-    }
-
-    p.mousePressed = function() {
-        p.draw();
+        p.canvas.auto_scale_draw(N);
     }
 };
 
-var my_sketch = new p5(sketch, 'zoom-test')
+new p5(sketch, 'zoom-test')
