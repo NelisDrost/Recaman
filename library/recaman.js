1d7a8da1bf42005e0deffb7f2474f0c1bce14f9b
Cornelis Drost <seregon888@gmail.com>
2021-12-16 19:46:22 +1300

    REC-2 Sequence Generator
    * Implement sequence super-class
    * Track steps taken between each Recaman term
    * Color each term in sketch by whether it's up or down

--- library/recaman.js
+++ library/recaman.js
@@ -4,19 +4,75 @@ if (!Array.prototype.last){
     };
 }
 
-class Recaman {
+/**
+ * Abstract base clase for number sequences
+ * @class Sequence
+ */
+class Sequence {
+    constructor() {
+        if (this.constructor === Sequence) {
+            throw new Error("Abstract base class can't be instantiated");
+        }
+        this.sequence = [];
+    }
+
+    /**
+     * Returns the length of the generated sequence
+     * @returns {number}
+     */
+    length() {
+        return this.sequence.length;
+    }
+
+    /**
+     * Generates missing terms up to the n'th term
+     * @param n
+     */
+    generate(n) {
+        throw new Error("Not implemented in abstract class");
+    }
+
+    /**
+     * Returns either a single value from the sequence, or a slice
+     * @param start
+     * @param end
+     */
+    get(start, end) {
+        return this._get_from_sequence(start, end, this.sequence)
+    }
+
+    _get_from_sequence(start, end, sequence) {
+        // Generate extra values if needed
+        this.generate(Math.max(start, end))
+
+        // Return either slice or single value as requested
+        if (end !== null) {
+            return sequence.slice(start, end)
+        } else {
+            return sequence[start]
+        }
+    }
+}
+
+class Recaman extends Sequence {
     constructor(n) {
-        this.sequence = [0];
+        super();
+        // First term in the sequence
+        this.sequence.push(0);
         this.used = new Set();
         this.used.add(0);
 
+        // The step to the next term
         this.j = 1;
+        this.steps = [0];
+
         if (n !== null) {
             this.generate(n);
         }
     }
 
     generate(n) {
+        // Skip generation if we've already reached n
         if (this.j >= n) {
             return;
         }
@@ -25,24 +81,21 @@ class Recaman {
         for (;this.j < n; this.j++) {
             if ((i - this.j) > 0 && !this.used.has(i-this.j)) {
                 i = i - this.j;
+                this.steps.push(-this.j);
             } else {
                 i = i + this.j;
+                this.steps.push(this.j);
             }
             this.sequence.push(i);
             this.used.add(i);
         }
     }
 
-    get(start, end) {
-        // Generate extra values if needed
-        this.generate(Math.max(start, end))
-
-        // Return either slice or single value as requested
-        if (end !== null) {
-            return this.sequence.slice(start, end)
-        } else {
-            return this.sequence[start]
-        }
+    /**
+     * Gets the step (or steps) taken to reach each term in the sequence
+     */
+    get_step(start, end) {
+        return this._get_from_sequence(start, end, this.steps);
     }
 }
 
--- sketches/sequenceTest.js
+++ sketches/sequenceTest.js
@@ -12,18 +12,30 @@ let sketch = function(p) {
     p.scroll_sequence = function () {
         // Displays a scrolling list of sequence values
         p.background(100);
-        let N = p.millis() / 100;
+        p.textSize(20);
+        p.textAlign(p.RIGHT, p.TOP)
+
+        // Pause briefly at the start
+        let N = Math.max(0, -20 + p.millis() / 100);
+
+        // Rows/columns of sequence terms
         let cols = 13;
         let rows = 32;
         let start = p.floor(N / cols) * cols;
 
-        p.textSize(20);
-        p.textAlign(p.RIGHT, p.TOP)
-
         for (let i = start; i < start + rows * cols; i+=cols) {
+            // Get 1 row at a time
+            let arr = p.sequence.get(i, i + cols)
+            let stp = p.sequence.get_step(i, i + cols)
+
             for (let j = 0; j < cols; j++) {
-                let arr = p.sequence.get(i, i + cols)
-                p.text(arr[j], 60 * (j + 1), 2 * (i - start - (N % cols)))
+                // Color steps down in red
+                if (stp[j] > 0) {
+                    p.stroke(0);
+                } else {
+                    p.stroke(255, 0, 0);
+                }
+                p.text(arr[j], 60 * (j + 1), 5 + 2 * (i - start - (N % cols)))
             }
         }
     }
